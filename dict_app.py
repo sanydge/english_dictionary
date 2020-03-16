dd666b35ca1e5478f51edc30abeb05224b8e96fa
sanydge <sanydge@gmail.com>
2020-03-16 15:44:33 +0200

    similarity ratio between two words

--- dict_app.py
+++ dict_app.py
@@ -1,10 +1,20 @@
+import difflib
 import json
+from difflib import SequenceMatcher
+
+SequenceMatcher(None, "rainn", "rain").ratio()
 
 data = json.load(open("data.json"))
 
 
-def translate(word):
-    return data[word]
+def translate(w):
+    w = w.lower()
+    if w in data:
+        return data[w]
+    elif w.capitalize() in data:
+        return data[w.capitalize()]
+    else:
+        return "The word doesn't exist. Please double check it."
 
 
 word = input("Enter word: ")
